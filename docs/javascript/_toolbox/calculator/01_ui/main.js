358c185e3ebd1c42100eb0caead0f79f2ee6abfc
nonaka101 <135217363+nonaka101@users.noreply.github.com>
2024-04-05 13:08:47 +0900

    update calc-ui

--- docs/javascript/_toolbox/calculator/01_ui/main.js
+++ docs/javascript/_toolbox/calculator/01_ui/main.js
@@ -1,28 +1,20 @@
-/* ≡≡≡ ▀▄ 「メニュー」ボタン ▀▄ ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-	■ 概要
-		モバイル画面時、ヘッダーにある「メニュー」ボタンを押すと、モーダルダイアログを表示
-	■ 備考
-		`showModal()` を使っているため、HTML側で autofocus 属性は不要
-		（フォーカスは、自動で近くのフォーカス可能な要素に移るため「閉じる」ボタンが選択される）
----------------------------------------------------------------------------- */
-
 // 各種要素の取得
-const menuBtn = document.getElementById('js_btn_calc');
-const menuDialog = document.getElementById('menu');
+const btnCalculator = document.getElementById('js_btn_calc');
+const dialogCalculator = document.getElementById('js_dialog_calculator');
 
 // ボタン押下時、ダイアログをモーダル状態で表示
-menuBtn.addEventListener('click', () => {
-	menuDialog.showModal();
+btnCalculator.addEventListener('click', () => {
+	dialogCalculator.showModal();
 });
 
 // ダイアログを閉じる（ダイアログ内 buttonタグの `onClick` イベントに使用）
-function closeDialog(){
-	menuDialog.close();
+function closeDialogCalculator(){
+	dialogCalculator.close();
 };
 
 // モーダルダイアログ外をクリック時、ダイアログを閉じる
-menuDialog.addEventListener('click', (e) => {
-	if(e.target === menuDialog){
-		menuDialog.close();
+dialogCalculator.addEventListener('click', (e) => {
+	if(e.target === dialogCalculator){
+		dialogCalculator.close();
 	}
 });
